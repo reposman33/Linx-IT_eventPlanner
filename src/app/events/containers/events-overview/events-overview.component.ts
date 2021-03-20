603bf2c6125af447179d4170b109a92b93a9c8c3
Marc Bakker <webdev@marcbakker.com>
2021-03-20 16:49:07 +0100

    Add events table.

--- src/app/events/containers/events-overview/events-overview.component.ts
+++ src/app/events/containers/events-overview/events-overview.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import  *  as  allEventsJsonData  from  '../../data/allEvents.json';
+import *  as  allEventsJsonData from '../../data/allEvents.json';
 
 @Component({
-  selector: 'app-events-overview',
-  templateUrl: './events-overview.component.html',
-  styleUrls: ['./events-overview.component.scss']
+	selector: 'app-events-overview',
+	templateUrl: './events-overview.component.html',
+	styleUrls: ['./events-overview.component.scss']
 })
 export class EventsOverviewComponent implements OnInit {
+	allEvents = [];
+	columnNames: string[];
+	displayDateFormat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-  allEvents = [];
+	constructor() { }
 
-  constructor() { }
+	ngOnInit(): void {
+		this.allEvents = (allEventsJsonData as any).default.events;
+		this.columnNames = Object.keys(this.allEvents[0]);
+		this.columnNames.splice(0, 1);
+	}
 
-  ngOnInit(): void {
-    this.allEvents = (allEventsJsonData as any).default;
-  }
+	formatDate(date: Date): string {
+		return new Date(date).toLocaleDateString(navigator.language, this.displayDateFormat);
+	}
 
 }
